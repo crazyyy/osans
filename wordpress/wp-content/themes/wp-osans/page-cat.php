7124d904af389f78b84b91cfaabb515af2a6de2a
crazyyy <crazyyy@gmail.com>
2015-10-17 14:52:19 +0300

    prod cat prerere

--- wordpress/wp-content/themes/wp-osans/page-cat.php
+++ wordpress/wp-content/themes/wp-osans/page-cat.php
@@ -6,39 +6,92 @@
 
         <div class="block-select">
           <h3>Подбор ИБП</h3>
-          <form action=""></form>
+          <form action="">
+
+          </form>
         </div><!-- /.block-select -->
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <?php the_content(); ?>
-        </article>
-        <div class="product-listing">
-          <?php if(get_field('second-title')) { echo '<h3 class="product-listing-title">' . get_field('second-title') . '</h3>'; } ?>
+        <div id="post-<?php the_ID(); ?>" <?php post_class('category-listing'); ?>>
 
           <?php
           $posts = get_field('manufacturer');
           if( $posts ): ?>
-            <ul class="product-listing-container">
+            <ul class="category-listing-container">
             <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
-                <?php setup_postdata($post); ?>
-                  <li class="product-item">
-                    <a href="<?php the_permalink(); ?>">
-                      <span>
-                        <?php if ( has_post_thumbnail()) :
-                          the_post_thumbnail('medium');
-                        else: ?>
-                          <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
-                        <?php endif; ?>
-                      </span>
-                      <h2><?php the_title(); ?></h2>
-                    </a>
-                  </li><!-- /.product-item -->
+              <?php setup_postdata($post); ?>
+
+                <li class="category-item">
+                  <a class="" href="<?php the_permalink(); ?>">
+                    <?php if ( has_post_thumbnail()) :
+                      the_post_thumbnail('medium');
+                    else: ?>
+                      <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                    <?php endif; ?>
+                  </a>
+                  <h2><?php the_title(); ?></h2>
+                  <?php if(get_field('description')) { echo '<h3 class="category-listing-title">' . get_field('description') . '</h3>'; } ?>
+                  <?php if(get_field('full-descr')) { echo '<p>' . get_field('full-descr') . '</p>'; } ?>
+                </li><!-- /.category-item -->
+
+
+                <li class="category-item">
+                  <a class="" href="<?php the_permalink(); ?>">
+                    <?php if ( has_post_thumbnail()) :
+                      the_post_thumbnail('medium');
+                    else: ?>
+                      <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                    <?php endif; ?>
+                  </a>
+                  <h2><?php the_title(); ?></h2>
+                  <?php if(get_field('description')) { echo '<h3 class="category-listing-title">' . get_field('description') . '</h3>'; } ?>
+                  <?php if(get_field('full-descr')) { echo '<p>' . get_field('full-descr') . '</p>'; } ?>
+                </li><!-- /.category-item -->
+                <li class="category-item">
+                  <a class="" href="<?php the_permalink(); ?>">
+                    <?php if ( has_post_thumbnail()) :
+                      the_post_thumbnail('medium');
+                    else: ?>
+                      <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                    <?php endif; ?>
+                  </a>
+                  <h2><?php the_title(); ?></h2>
+                  <?php if(get_field('description')) { echo '<h3 class="category-listing-title">' . get_field('description') . '</h3>'; } ?>
+                  <?php if(get_field('full-descr')) { echo '<p>' . get_field('full-descr') . '</p>'; } ?>
+                </li><!-- /.category-item -->
+                <li class="category-item">
+                  <a class="" href="<?php the_permalink(); ?>">
+                    <?php if ( has_post_thumbnail()) :
+                      the_post_thumbnail('medium');
+                    else: ?>
+                      <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                    <?php endif; ?>
+                  </a>
+                  <h2><?php the_title(); ?></h2>
+                  <?php if(get_field('description')) { echo '<h3 class="category-listing-title">' . get_field('description') . '</h3>'; } ?>
+                  <?php if(get_field('full-descr')) { echo '<p>' . get_field('full-descr') . '</p>'; } ?>
+                </li><!-- /.category-item -->
+                <li class="category-item">
+                  <a class="" href="<?php the_permalink(); ?>">
+                    <?php if ( has_post_thumbnail()) :
+                      the_post_thumbnail('medium');
+                    else: ?>
+                      <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                    <?php endif; ?>
+                  </a>
+                  <h2><?php the_title(); ?></h2>
+                  <?php if(get_field('description')) { echo '<h3 class="category-listing-title">' . get_field('description') . '</h3>'; } ?>
+                  <?php if(get_field('full-descr')) { echo '<p>' . get_field('full-descr') . '</p>'; } ?>
+                </li><!-- /.category-item -->
+
+
+
+
               <?php endforeach; ?>
-              </ul><!-- /.product-listing-container -->
+              </ul><!-- /.category-listing-container -->
               <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
           <?php endif; ?>
 
-        </div><!-- /.product-listing -->
+        </div><!-- /.category-listing -->
       <?php endwhile; else: ?>
         <article>
           <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
