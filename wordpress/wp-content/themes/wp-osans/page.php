861f2ef11610dbdec2c4b4f100fab30dace206d4
crazyyy <crazyyy@gmail.com>
2015-10-17 12:01:41 +0300

    page

--- wordpress/wp-content/themes/wp-osans/page.php
+++ wordpress/wp-content/themes/wp-osans/page.php
@@ -1,18 +1,22 @@
 <?php get_header(); ?>
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <div class="row inner">
+    <div class="col-md-8">
+      <h1 class="title3"><?php the_category(', '); ?></h1>
 
-      <h1 class="page-title inner-title"><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-      <?php edit_post_link(); ?>
+      <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+        <h1 class="single-title inner-title title3"><?php the_title(); ?></h1>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <?php the_content(); ?>
+        </article>
+      <?php endwhile; else: ?>
+        <article>
+          <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
+        </article>
+      <?php endif; ?>
 
-    </article>
-  <?php endwhile; else: // If 404 page error ?>
-    <article>
+      <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
 
-      <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
-
-    </article>
-  <?php endif; ?>
-<?php get_sidebar(); ?>
+    </div><!-- /.col-md-8 -->
+    <?php get_sidebar(); ?>
+  </div><!-- /.row inner -->
 <?php get_footer(); ?>
